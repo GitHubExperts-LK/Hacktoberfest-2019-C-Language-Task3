7a391938269ca2f0ab8554b3ff27de90582a90c2
AdeeSL <49285003+AdeeSL@users.noreply.github.com>
2019-10-23 18:31:23 +0530

    completed task 1 & task 2

--- array.c
+++ array.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-void  main()
+int main()
 {
-    
+    int size,x,y;
+    printf("Enter Array Size ");
+    scanf("%d",&size);
+    int arr[size];
+    for(x=0;x<size;x++){
+        printf("element - %d : ",x);
+        scanf("%d",&arr[x]);
+    }
+    printf("The values store into the array are : ");
+    for(x=0;x<size;x++){
+        printf("%d ",arr[x]);
+    }
+
+    printf("The values store into the array in reverse are : ");
+    for(x=size-1;x>=0;x--){
+        printf("%d ",arr[x]);
+    }
 }
