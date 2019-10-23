f8e42b63cc5f185e63bbefe162a3546f6ef31e06
dilshan97 <dilshanramesh81@gmail.com>
2019-10-23 16:09:49 +0530

    task 1 compelted

--- array.c
+++ array.c
@@ -2,5 +2,13 @@
 
 void  main()
 {
-    
+    int num[10];
+
+    for(int i=0; i < 10; i++){
+        scanf("%d", &num[i]);
+    }
+
+    for(int i=0; i < 10; i++){
+        printf(num[i]);
+    }
 }
