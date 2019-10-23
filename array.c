549d534055421e9c677d2d054d805f0fc166b334
SrilalS <Srilalsachintha@Gmail.com>
2019-10-23 16:25:12 +0530

    Update array.c

--- array.c
+++ array.c
@@ -2,5 +2,17 @@
 
 void  main()
 {
-    
+    int Arr[10];
+    printf("Enter 10 Numbers seperated by spaces");
+    scanf(" %d %d %d %d %d %d %d %d %d %d");
+
+    for(int i = 0; i<10;i++){
+        printf(Arr[i]);
+        printf("\n");
+    }
+
+    for(int i = 9; i<0;i--){
+        printf(Arr[i]);
+        printf("\n");
+    }
 }
