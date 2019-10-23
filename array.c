cc5be490e7a2aba3218be1928c4879fb98cd1736
KavindyaSathsarani <56838207+KavindyaSathsarani@users.noreply.github.com>
2019-10-23 20:58:58 +0530

    modified

--- array.c
+++ array.c
@@ -2,5 +2,16 @@
 
 void  main()
 {
-    
+  int n[10];
+  int i;
+  for(i=0;i<10;i++)
+  {
+      printf("Enter element %d : ",i+1);
+      scanf("%d",&n[i]);
+  }
+  printf("\n Elements in array are ");
+  for(i=0;i<10;i++)
+  {
+      printf("%d ",n[i]);
+  }
 }
